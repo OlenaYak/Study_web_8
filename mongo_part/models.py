45bff1c0b5fc7341b7b5be37f158fd5aef03ed55
Olena Yakovchuk <124076979+OlenaYak@users.noreply.github.com>
2025-07-09 22:33:43 +0200

    Update models.py

--- mongo_part/models.py
+++ mongo_part/models.py
@@ -1,6 +1,6 @@
 from mongoengine import Document, StringField, ListField, ReferenceField, connect
 
-connect(db="web25", host="mongodb+srv://userweb25:567234@clusterolena.onftjc3.mongodb.net/?retryWrites=true&w=majority&appName=ClusterOlena")
+connect(db="web25", host="mongodb+srv://userweb25:*****@clusterolena.onftjc3.mongodb.net/?retryWrites=true&w=majority&appName=ClusterOlena")
 
 class Author(Document):
     fullname = StringField(required=True, max_length=120)
