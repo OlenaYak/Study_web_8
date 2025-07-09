6cd49edfbf8132a1a5b0046dd1dc7f24c8a3e350
Olena Yakovchuk <124076979+OlenaYak@users.noreply.github.com>
2025-07-09 22:34:16 +0200

    Update models.py

--- rabbit_part/models.py
+++ rabbit_part/models.py
@@ -2,7 +2,7 @@ from mongoengine import connect, disconnect, Document, StringField, BooleanField
 
 def connect_db():
     disconnect(alias='default')
-    connect(db="web25", host="mongodb+srv://userweb25:567234@clusterolena.onftjc3.mongodb.net/?retryWrites=true&w=majority&appName=ClusterOlena")
+    connect(db="web25", host="mongodb+srv://userweb25:*****@clusterolena.onftjc3.mongodb.net/?retryWrites=true&w=majority&appName=ClusterOlena")
 
 
 class Contact(Document):
